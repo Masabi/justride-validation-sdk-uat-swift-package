6789309f937c4b0b9ece9428ea740327be676e8c
Native Platforms <native.platforms@masabi.com>
2026-04-01 11:55:16 +0200

    [Update] JustRide Validation SDK (5.41.0-2cbf810e-SNAPSHOT)

--- Package.swift
+++ Package.swift
@@ -28,13 +28,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "JustrideValidationSDK",
-            url: "https://artifactory2.masabi.com/artifactory/libs-snapshot-local/com/masabi/validation/JustrideValidationSDK-ios-UAT/5.41.0-b69dc40a-SNAPSHOT/JustrideValidationSDK-ios-UAT-5.41.0-b69dc40a-20260401.071347-1.zip",
-            checksum: "1d1c05492df88c856fef8b0cf036ee07f6ae727a28d3f40a8bb82a1aecc48957"
+            url: "https://artifactory2.masabi.com/artifactory/libs-snapshot-local/com/masabi/validation/JustrideValidationSDK-ios-UAT/5.41.0-2cbf810e-SNAPSHOT/JustrideValidationSDK-ios-UAT-5.41.0-2cbf810e-20260401.083659-1.zip",
+            checksum: "d21562327c257c14bb0802dff66fd668dc794dc2c525aabe5b7c93617fb02b09"
         ),
         .binaryTarget(
             name: "mbedtls",
-            url: "https://artifactory2.masabi.com/artifactory/libs-snapshot-local/com/masabi/validation/mbedtls-ios/5.41.0-b69dc40a-SNAPSHOT/mbedtls-ios-5.41.0-b69dc40a-20260401.072312-1.zip",
-            checksum: "b27ed24e262312a0cdf7714ec213a30ac0013d91877c751ac6ba857edfbf5353"
+            url: "https://artifactory2.masabi.com/artifactory/libs-snapshot-local/com/masabi/validation/mbedtls-ios/5.41.0-2cbf810e-SNAPSHOT/mbedtls-ios-5.41.0-2cbf810e-20260401.091343-1.zip",
+            checksum: "4b387ce6bb854773e84696df5cc8c18673fd5a61dcd5b1ce2e61efb9a6a58324"
         )
 
     ]
